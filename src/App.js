afe89ebaa7945db2a3cde74cbc3a718617dc008d
unknown <samuelsc2518@gmail.com>
2021-09-09 16:29:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { Fragment } from "react"
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
+
+  const [busqueda, setBusqueda] = useState({ 
+    ciudad: '',
+    pais: ''
+ })
+
+  const [consultar, setConsultar] = useState(false)
+
+  const { ciudad, pais } = busqueda
+
+  useEffect(() => {
+    const consultarApi = async () => {
+
+      const appId = "25a4e857ec9d8e049bb58ecedd5e90c5"
+
+      const url = `api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+
+      const respuesta = await fetch(url)
+      const resultado = await respuesta.json()
+
+      console.log(resultado)
+    }
+    consultarApi()
+  }, [consultar])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header
+        titulo="Clima React App"
+      />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario 
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+
+function Formulario({ busqueda, setBusqueda, setConsultar }) {
+    
+    
+
+     const [error, setError] = useState(false)
+
+     //extraer ciudad y pais
+
+     const { ciudad, pais } = busqueda
+
+     //funcion que lee los datos de los formularios
+
+     const handleChange = e => {
+         //actualiza el state
+
+         setBusqueda({
+             ...busqueda,
+             [e.target.name] : e.target.value
+         })
+     }
+     const handleSubmit = e =>{
+         e.preventDefault()
+
+         //validar
+         if(ciudad.trim() === '' || pais.trim() === ''){
+             setError(true)
+             return
+         }
+
+         setError(false)
+
+         setConsultar(true)
+
+
+     }
+
+    return (
+        <form 
+          action=""
+          onSubmit={handleSubmit}
+        >
+            {error ? <p className="error red darken-4">Todos los campos son obligatorios</p> : null }
+            <div className="input-field col s12">
+                <input 
+                  type="text"
+                  name="ciudad"
+                  id="ciudad"
+                  value={ciudad}
+                  onChange={e => handleChange(e)}
+                />
+                <label htmlFor="ciudad">Ciudad: </label>
+            </div>
+            <div className="input-field col s12">
+                <select 
+                  name="pais" 
+                  id="pais"
+                  value={pais}
+                  onChange={e => handleChange(e)}
+                >
+                    <option value="">-- Seleccione un país --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                    <option value="VE">Venezuela</option>
+                </select>
+                <label htmlFor="pais">País: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <input 
+                  type="submit" 
+                  value="Buscar Clima"
+                  className="waves-effect waves-light btn btn-large btn-block yellow accent-4" 
+                />
+            </div>
+        </form>
+    )
+}
+
+export default Formulario
